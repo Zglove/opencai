7719d46dd25a2d169098dfccf17901ae7db0fef3
谈虎啸 <414384153@qq.com>
2017-01-23 09:10:10 +0800

    1.导入cocoaPods管理工具 2.导入AFNetworking、SDWebImage两个第三方库

---
+++ OpenCai/Pods/Target Support Files/Pods-OpenCai/Pods-OpenCai-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OpenCai : NSObject
+@end
+@implementation PodsDummy_Pods_OpenCai
+@end
---
+++ OpenCai/Pods/Target Support Files/Pods-OpenCai/Pods-OpenCai-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_OpenCaiVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_OpenCaiVersionString[];
+
